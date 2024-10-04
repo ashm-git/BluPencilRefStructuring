fc841610fa04eb9a737314af3bc9059c0818041a
ashokkumara <88478058+ashm-git@users.noreply.github.com>
2024-10-04 10:27:00 +0530

    Delete RefBOT directory

--- RefBOT/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace RefBOT.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- RefBOT/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace RefBOT.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
