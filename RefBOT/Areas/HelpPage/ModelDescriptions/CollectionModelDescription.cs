d9aaddc616d374422aa4c49c3e12279ba75f6222
ashokkumara <88478058+ashm-git@users.noreply.github.com>
2024-10-04 10:28:37 +0530

    Delete RefBOT directory

--- RefBOT/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace RefBOT.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
